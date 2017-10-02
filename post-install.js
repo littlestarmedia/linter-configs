6c04a46d1860ca54e2195c35d08d155f0ab42004
James Husband <james.husband@littlestarmedia.com>
2017-10-02 16:16:44 +0100

    Added Linter Configs

---
+++ post-install.js
@@ -0,0 +1,8 @@
+
+
+const chalk = require('chalk');
+const term = require('terminal-kit').terminal;
+
+
+term.bold.red('Install Complete\n');
+term.bold.yellow('Please ensure you now install ES-LINTER for your IDE\n');
