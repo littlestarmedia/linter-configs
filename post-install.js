2d6f95b33ac1f55a65e302af96aafb2d947b9bb4
James Husband <james.husband@littlestarmedia.com>
2017-10-03 15:17:03 +0100

    Removed unused package "chalk"

--- post-install.js
+++ post-install.js
@@ -1,8 +1,5 @@
 
-
-const chalk = require('chalk');
 const term = require('terminal-kit').terminal;
-
 
 term.bold.red('Install Complete\n');
 term.bold.yellow('Please ensure you now install ES-LINTER for your IDE\n');
